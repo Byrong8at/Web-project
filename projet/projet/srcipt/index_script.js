b5ca846b6c5ec63781552a3427d58592dac892bb
Shown <106871132+Byrong8at@users.noreply.github.com>
2024-03-10 12:50:37 +0100

    Add files via upload

---
+++ projet/projet/srcipt/index_script.js
@@ -0,0 +1,8 @@
+function dropdown() {
+        document.getElementById("croix").style.display = "block";
+        document.getElementById("dropbtn").style.display = "none";
+        document.getElementById("myDropdown").style.display = "block";    
+    }
+
+
+  
