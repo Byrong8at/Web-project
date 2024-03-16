62bbc1597b9094ef6d80ce21207d61e5ab917280
Shown <hugomjh3012@gmail.com>
2024-03-16 16:40:41 +0100

    Update
    Finish the home page at 95% just need to do the menu when we click on.
    And possible graphics retouch, idk if i finish him tomorow or i start the entreprise page
    
    Co-Authored-By: ShibaCode <105083217+SnoobySN@users.noreply.github.com>

--- projet/projet/srcipt/index_script.js
+++
@@ -1,8 +0,0 @@
-function dropdown() {
-        document.getElementById("croix").style.display = "block";
-        document.getElementById("dropbtn").style.display = "none";
-        document.getElementById("myDropdown").style.display = "block";    
-    }
-
-
-  
---
+++ projet/projet/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        'custom-blue': '#4C9094',
+        'custom-green': '#164988',
+        'custom-purple': '#567BB2',
+      },
+    },
+  },
+  plugins: [],
+}
+
