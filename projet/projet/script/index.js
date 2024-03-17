a2eb5c83dc956f05c4ca2eeeba31757b427ef770
Shown <hugomjh3012@gmail.com>
2024-03-17 20:08:13 +0100

    update and add
    just page error like the index page
    correction with media query and a js script who help to the redirection with button

---
+++ projet/projet/script/index.js
@@ -0,0 +1,11 @@
+
+try{
+document.getElementById("retour").addEventListener("click", function() {
+    window.location.href = "acceuil.html";
+});}
+catch{}
+try{
+document.getElementById("connexion").addEventListener("click", function() {
+    window.location.href = "connexion.html";
+});}
+catch{}
