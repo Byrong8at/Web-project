ee57662b9e6f74f6bbe525a77223dfbfccdc8f0d
vShown <hugomjh3012@gmail.com>
2024-03-26 09:09:58 +0100

    test
    aa

--- projet/projet/connexion.php
+++ projet/projet/connexion.php
@@ -2,6 +2,23 @@
 require_once(dirname(__FILE__) .'/bdd.php');
 session_start();
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $identifiant = $_POST['identifiant'];
+    $mot_de_passe = $_POST['mot_de_passe'];
+    login($identifiant, $mot_de_passe, $conn);
+}
+
+function login($identifiant, $mot_de_passe, $conn) {
+    $sqllog = "SELECT * FROM utilisateur WHERE Login = ? AND Mot_de_passe = ?";
+    $quezy = $conn->prepare($sqllog);
+    $quezy->execute([$identifiant, $mot_de_passe]);
+    $user = $quezy->fetch(PDO::FETCH_ASSOC);
+    if ($user) {
+        header('Location: acceuil.html');
+    } else {
+        echo "Votre identifiant ou mot de passe ne correspond pas";
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -28,37 +45,16 @@ session_start();
             <p></p>
 
             <h4 class="info_connexion">Identifiant</h4>
-            <input button name="identifiant" class="info_button">
+            <input type="text" name="identifiant" class="info_button">
             <h4 class="info_connexion">Mot de passe</h4>
-            <input button name="mot_de_passe"  type="password" class="info_button">
+            <input type="password" name="mot_de_passe" class="info_button">
             <br>
             <button class="cesi"><img src="src/cesi.png" alt="logo de cesi" id="logo_button"> Connection avec L'ent Cesi</button>
             <br>
-            <button class="cesi"> Se connecter</button>
+            <button type="submit" class="cesi"> Se connecter</button>
             <p></p>
             <i>problème de connexion</i>
         </form>
-        
-        
-        
     </main>
 </body>
 </html>
-
-<?php
-$identifiant = $_POST['identifiant'];
-$mot_de_passe = $_POST['mot_de_passe'];
-login($identifiant, $mot_de_passe,$conn);
-function login($identifiant, $mot_de_passe,$conn) {
-    $sqllog = "SELECT * FROM utilisateur WHERE Login = ? AND Mot_de_passe = ?";
-    $quezy = $conn->prepare($sqllog);
-    $quezy->execute([$identifiant,$mot_de_passe]);
-    $user = $quezy->fetch(PDO::FETCH_ASSOC);
-    if ($user) {
-        header('Location:acceuil.html');
-    }else {
-        echo "Votre identifiant ou mot de passe ne correspond pas";
-    }
-
-}
-
--- projet/projet/script/index.js
+++ projet/projet/script/index.js
@@ -1,11 +1,6 @@
 
 try{
-document.getElementById("retour").addEventListener("click", function() {
-    window.location.href = "acceuil.html";
-});}
-catch{}
-try{
 document.getElementById("connexion").addEventListener("click", function() {
-    window.location.href = "connexion.html";
+    window.location.href = "connexion.php";
 });}
 catch{}
