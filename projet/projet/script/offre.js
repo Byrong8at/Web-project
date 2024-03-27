5fff65117271953406cef86a08b802deb4eab952
ShibaDeLaStreet <105083217+SnoobySN@users.noreply.github.com>
2024-03-27 19:00:40 +0100

    Changement de pc

--- projet/projet/script/offre.js
+++ projet/projet/script/offre.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', function() {
     let localisation = document.getElementById("localisation");
     let domaine = document.getElementById("domaine");
     let contrat = document.getElementById("contrat");
-    let date = document.getElementById("date");
     let trier = document.getElementById("trier");
     let filtrermobile = document.getElementById("filtrermobile");
     let triermobile = document.getElementById("triermobile");
@@ -13,16 +12,45 @@ document.addEventListener('DOMContentLoaded', function() {
         if (L[i]) {
             L[i].addEventListener("click", function() {
                 console.log("ok");
-                if (document.getElementsByClassName("allfiltri")[i].hidden == true){
-                    document.getElementsByClassName("allfiltri")[i].hidden = false;
-                }else{
-                    document.getElementsByClassName("allfiltri")[i].hidden = true;
-                    
-                }
-                
+                document.getElementsByClassName("allfiltri")[i].classList.toggle("hidden");
             });
         } else {
             console.log("Erreur");
         }
     }
+
+    let selectdate = document.getElementsByClassName("selectdate");
+    let selectoffre = document.getElementsByClassName("selectoffre");
+    let selectentreprise = document.getElementsByClassName("selectentreprise");
+    let selectcontrat = document.getElementsByClassName("selectcontrat");
+    let selectdureecontrat = document.getElementsByClassName("selectdureecontrat");
+    let bac0 = document.getElementsByClassName("bac0");
+    let bac1 = document.getElementsByClassName("bac1");
+    let bac2 = document.getElementsByClassName("bac2");
+    let bac3 = document.getElementsByClassName("bac3");
+    let bac4 = document.getElementsByClassName("bac4");
+    let bac5 = document.getElementsByClassName("bac5");
+    let inputville = document.getElementsByClassName("inputville");
+    let remuneration = document.getElementsByClassName("remuneration");
+    let teletravail = document.getElementsByClassName("teletravail");
+    let generaliste = document.getElementsByClassName("generaliste");
+    let informatique = document.getElementsByClassName("informatique");
+    let btp = document.getElementsByClassName("btp");
+    let s3e = document.getElementsByClassName("selectdureecontrat");
+
+    Lselect = [selectdate,selectoffre,selectentreprise,selectcontrat,selectdureecontrat];
+    Lbox = [bac0,bac1,bac2,bac3,bac4,bac5,remuneration,teletravail,generaliste,informatique,btp,s3e];
+
+    for(let i = 0; i < Lselect.length; i++){
+        L[i].addEventListener('change', (event) => {
+            select2.value = event.target.value;
+          });
+    }
+    selectdate.addEventListener('change', (event) => {
+        select2.value = event.target.value;
+      });
+
+    input1.addEventListener('input', (event) => {
+        input2.value = event.target.value;
+      });
 });
