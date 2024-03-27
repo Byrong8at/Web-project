5750008e82826b623b5a65bc103672d856f7ee91
ShibaDeLaStreet <105083217+SnoobySN@users.noreply.github.com>
2024-03-27 22:42:11 +0100

    Manque plus que les finitions
    Tout est good manque plus qu'implémenter qq fonction js et le php

--- projet/projet/script/offre.js
+++ projet/projet/script/offre.js
@@ -10,9 +10,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
     for (let i = 0; i < L.length; i++) {
         if (L[i]) {
-            L[i].addEventListener("click", function() {
-                console.log("ok");
-                document.getElementsByClassName("allfiltri")[i].classList.toggle("hidden");
+            L[i].addEventListener("click", (event) => {
+                if (event.target == filtrermobile){
+                    filtrermobile.classList.toggle("on");
+                    triermobile.classList.remove("on");
+                    document.getElementsByClassName("allfiltri")[0].classList.toggle("hidden");
+                    document.getElementsByClassName("allfiltri")[1].classList.add("hidden");
+                }
+                else if (event.target == triermobile){
+                    triermobile.classList.toggle("on");
+                    filtrermobile.classList.remove("on");
+                    document.getElementsByClassName("allfiltri")[1].classList.toggle("hidden");
+                    document.getElementsByClassName("allfiltri")[0].classList.add("hidden");
+                }
+                else{
+                    document.getElementsByClassName("allfiltri")[i].classList.toggle("hidden");
+                }
             });
         } else {
             console.log("Erreur");
@@ -38,19 +51,63 @@ document.addEventListener('DOMContentLoaded', function() {
     let btp = document.getElementsByClassName("btp");
     let s3e = document.getElementsByClassName("selectdureecontrat");
 
-    Lselect = [selectdate,selectoffre,selectentreprise,selectcontrat,selectdureecontrat];
-    Lbox = [bac0,bac1,bac2,bac3,bac4,bac5,remuneration,teletravail,generaliste,informatique,btp,s3e];
+    let Lselect = [selectdate,selectoffre,selectentreprise,selectcontrat,selectdureecontrat];
+    let Lbox = [bac0,bac1,bac2,bac3,bac4,bac5,remuneration,teletravail,generaliste,informatique,btp,s3e];
+    let Linput = [inputville];
 
-    for(let i = 0; i < Lselect.length; i++){
-        L[i].addEventListener('change', (event) => {
-            select2.value = event.target.value;
-          });
+    for (let i = 0; i < Lselect.length; i++) {
+        let selectElements = Lselect[i];
+        for (let j = 0; j < selectElements.length; j++) {
+            selectElements[j].addEventListener('change', (event) => {
+                if (Lselect[i][0] && Lselect[i][0].value !== event.target.value){
+                    Lselect[i][0].value = event.target.value;
+                }
+                if (Lselect[i][1] && Lselect[i][1].value !== event.target.value){
+                    Lselect[i][1].value = event.target.value;
+                }
+            });
+        }
     }
-    selectdate.addEventListener('change', (event) => {
-        select2.value = event.target.value;
-      });
 
-    input1.addEventListener('input', (event) => {
-        input2.value = event.target.value;
-      });
+    for (let i = 0; i < Lbox.length; i++) {
+        let selectElements = Lbox[i];
+        for (let j = 0; j < selectElements.length; j++) {
+            selectElements[j].addEventListener('click', (event) => {
+                if (Lbox[i][0] && Lbox[i][0].checked !== event.target.checked){
+                    Lbox[i][0].checked = event.target.checked;
+                }
+                if (Lbox[i][1] && Lbox[i][1].checked !== event.target.checked){
+                    Lbox[i][1].checked = event.target.checked;
+                }
+            });
+        }
+    }
+
+    for (let i = 0; i < Linput.length; i++) {
+        let selectElements = Linput[i];
+        for (let j = 0; j < selectElements.length; j++) {
+            selectElements[j].addEventListener('input', (event) => {
+                if (Linput[i][0] && Linput[i][0].value !== event.target.value){
+                    Linput[i][0].value = event.target.value;
+                }
+                if (Linput[i][1] && Linput[i][1].value !== event.target.value){
+                    Linput[i][1].value = event.target.value;
+                }
+            });
+        }
+    }
+
+    let coeur = document.getElementsByClassName("coeur");
+
+    for (let i = 0; i < coeur.length ; i++){
+        coeur[i].addEventListener('click', (event) => {
+            if (event.target.classList.contains("favorite")){
+                event.target.setAttribute("src", "src/coeur.png");
+            }else{
+                event.target.setAttribute("src", "src/coeurrempli.png");
+            }
+            event.target.classList.toggle("favorite");
+        });
+    }
+
 });
