cc25f944f813728272d3d1d62176adc219df2312
vShown <hugomjh3012@gmail.com>
2024-03-31 21:45:04 +0200

    Update offre.php
    realisation de la requete pour postuler

--- projet/projet/view/offre.php
+++ projet/projet/view/offre.php
@@ -1,7 +1,52 @@
 <?php require_once(dirname(__FILE__) .'/../controller/controller.php');
-redirection();?>
+redirection();
+$error_message = '';
+if (isset($_POST['postuler'])) {
+    $id_user=$_SESSION['user']['ID_user'];
+    $id_offre= $_GET['ID_offre'];
+    try {
+        if (verif($id_user,$id_offre, $conn)){
+            if (postuler($id_user,$id_offre, $conn)) {
+                $error_message ='';
+                
+            } else {
+                $error_message = "Une erreur à eu lieu";
+            }}
+        else {
+            $error_message = "Vous avez déjà postuler à cette offre";
+        }
+    } catch (PDOException $e) {
+        $error_message = "Échec de la connexion à la base de données : " . $e->getMessage();
+    }
+}
+
+
+function verif($id_user,$id_offre, $conn){
+    $sql = "SELECT *
+    FROM candidature
+    WHERE ID_user= :id_user AND ID_offre = :id_offre";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':id_user', $id_user);
+    $stmt->bindParam(':id_offre', $id_offre);
+    $stmt->execute();
+    $count = $stmt->fetchColumn();
+    if ($count == 0) {
+        return true;
+    }else{
+        return false;
+    }
+}
+function postuler($id_user,$id_offre,  $conn){
+    $sql ="INSERT INTO candidature(ID_offre, ID_user) VALUES (:id_offre, :id_user)";
+    $quezy = $conn->prepare($sql);
+    $quezy->bindParam(':id_offre', $id_offre);
+    $quezy->bindParam(':id_user', $id_user);
+    $quezy->execute();
+    return true;
+}
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -41,6 +86,13 @@ redirection();?>
             ?>
         </h2>
         <h2 class="text-2xl mx-4 py-4 md:text-3xl lg:text-4xl">Description : <?php echo $data_offre[0]['detail'];?></h2>
+        <form method="Post">
+        <?php
+            echo '<p name="error-message" style="color: red;">' . $error_message . '</p>';
+        if ($_SESSION['user']['Statut'] == 1){?>
+            <button type="submit" name="postuler" class="bg-custom-green px-8 py-8 flex justify-items-end text-white ml-auto mr-4 rounded-md">Postuler</button>
+            <?php } ?>
+        </form>
     </main>
 
     <?php require_once(dirname(__FILE__) .'/footer.php')?>
