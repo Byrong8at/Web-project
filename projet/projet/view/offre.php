e536ee94c4f673d0c860632d87372064d2eb2091
vShown <hugomjh3012@gmail.com>
2024-04-01 15:14:42 +0200

    Update offre.php

--- projet/projet/view/offre.php
+++ projet/projet/view/offre.php
@@ -91,9 +91,7 @@ function postuler($id_user,$id_offre,  $conn){
             ?>
         </h2>
         <?php
-            if($data_offre[0]['entreprise_Adresse'][0]['Adresse'] != ""){
-                echo "<h2 class='text-lg mx-4 py-4 md:text-lg lg:text-xl'>Adresse : " . $data_offre[0]['entreprise_Adresse'][0]['Adresse'] . "</h2>";
-            }
+            
             if($data_offre[0]['entreprise_Adresse'][0]['Adresse_2'] != ""){
                 echo "<h2 class='text-lg mx-4 py-4 md:text-lg lg:text-xl'>Adresse 2 : " . $data_offre[0]['entreprise_Adresse'][0]['Adresse_2'] . "</h2>";
             }
